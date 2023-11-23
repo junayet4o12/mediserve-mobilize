81cbfff220bafaf9482e5ce3853ccafd87910dba
junayet <muhammadjunayetmaruf@gmail.com>
2023-11-23 21:04:21 +0600

    general route setup has completed

---
+++ src/MyRouts/MyRouts.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+
+import { createBrowserRouter } from "react-router-dom";
+
+const MyRouts = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Hello world!</div>,
+    },
+]);
+
+export default MyRouts;
