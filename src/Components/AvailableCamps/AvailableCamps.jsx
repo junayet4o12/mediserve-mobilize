d4620455420361dfed6d3faa3fbd23d2571afe4e
junayet <muhammadjunayetmaruf@gmail.com>
2023-11-30 21:35:46 +0600

    finally completed

--- src/Components/AvailableCamps/AvailableCamps.jsx
+++ src/Components/AvailableCamps/AvailableCamps.jsx
@@ -6,19 +6,23 @@ import Title from "../Title/Title";
 import CampCard from "../../Home/PopularCamp/CampCard";
 import AvailableCampsCard from "./AvailableCampsCard";
 import { Helmet } from "react-helmet-async";
+import Loading from "../Loading";
 
 const AvailableCamps = () => {
     const axiosPublic = useAxiosPublic()
 
-    const { data: availableCamp = [] } = useQuery({
+    const { data: availableCamp = [], isLoading } = useQuery({
         queryKey: ['availableCamp'],
         queryFn: async () => {
             const res = await axiosPublic.get('/camps')
             return res?.data
         }
     })
+    if(isLoading){
+        return <Loading></Loading>
+    }
     return (
-        <div>
+        <div className="bg-blue-50">
             <Helmet>
                 <title>Mediserve Mobilize | Available Camp</title>
             </Helmet>
